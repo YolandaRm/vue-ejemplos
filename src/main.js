32b35ad9ffc667105ce2eafacd3592f913529f5c
YolandaRm <yramosmartin@gmail.com>
2020-07-14 22:33:04 +0200

    vue-ejemplos

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/* 
+  para iniciar una aplicación en Vue, 
+  creamos una nueva instancia
+  y el método render especficamos que componente pintar
+  esa instancia la pintamos en un elemento del DOM con el método $mount
+  que recibe un selector css para referenciar ese elemento 
+*/
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+ 
